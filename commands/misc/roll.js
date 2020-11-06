53727af800a3bc6eb8aa13b3994903d53c2a7844
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2020-11-06 16:37:18 -0600

    Update roll.js

--- commands/misc/roll.js
+++ commands/misc/roll.js
@@ -7,16 +7,14 @@ module.exports = {
     run: (message, args) => {
 
         if (args.length < 1) {
-
             let tempNum = Math.floor(Math.random() * 100);
             message.reply(`Rolling a random number between 1-100! \n You have rolled ${tempNum}!`);
-
         } else {
-
-            let x = args[0];
-            let tempNum = Math.floor(Math.random() * x);
-            message.reply(`Rolling a random number between 1-${x}! \n You have rolled ${tempNum}!`);
-
+            if(!args[0].isNaN()) {
+                let x = args[0];
+                let tempNum = Math.floor(Math.random() * x);
+                message.reply(`Rolling a random number between 1-${x}! \n You have rolled ${tempNum}!`);
+            }
         }
     },
 };
