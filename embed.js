aef299aeb0e19a7efcbc7bff34adfd5079c1cb5a
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2020-11-06 15:22:28 -0600

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,14 +1,6 @@
 const Discord = require('discord.js');
 
-new Discord.MessageEmbed()
+let embed = new Discord.MessageEmbed()
 .setTitle('Alliance Status:')
 .setDescription(`Below is the returned Status with alliance`)
-.addFields({
-    name: "Alliance Tag",
-    value: `test`
-})
-.addFields({
-    name: "Status with our alliance",
-    value: `test`
-})
 .setColor('BLUE')
