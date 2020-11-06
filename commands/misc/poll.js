0857d9d7c3e99838c078cad68d242ce4bf84b597
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2020-11-06 15:58:46 -0600

    Update poll.js

--- commands/misc/poll.js
+++ commands/misc/poll.js
@@ -18,7 +18,7 @@ module.exports = {
             .setDescription(pollDescription)
             .setColor('RED')
             .setTimestamp(Date.now())
-            .setFooter(message.author.bot.username, message.author.bot.avatarURL())
+            .setFooter(message.author.bot.username, message.author.bot.displayAvatarURL())
         let msgEmbed = await pollChannel.send(embedPoll);
         await msgEmbed.react('👍')
         await msgEmbed.react('👎')
