ef305c65eb7c6c744051a9cd01355402d297f38c
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2020-11-15 22:11:47 -0600

    Update welcome.js

--- features/welcome.js
+++ features/welcome.js
@@ -37,7 +37,7 @@ module.exports = (client) => {
     // Pick up the pen
 	ctx.beginPath();
 	// Start the arc to form a circle
-	ctx.arc(27, 27, 27, 0, Math.PI * 2, true);
+	ctx.arc(125, 125, 80, 0, Math.PI * 2, true);
 	// Put the pen down
 	ctx.closePath();
 	// Clip off the region you drew on
