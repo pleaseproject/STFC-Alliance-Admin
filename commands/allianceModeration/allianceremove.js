96148c4ea7a53ff528ae7cae7f3fad2aeb9be554
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2020-11-18 23:39:45 -0600

    Update allianceremove.js

--- commands/allianceModeration/allianceremove.js
+++ commands/allianceModeration/allianceremove.js
@@ -13,17 +13,16 @@ module.exports = {
         const guildId = message.guild.id;
         allianceId = args[0].toLowerCase();
 
-        await allianceSchema.deleteOne({
+        const results = await allianceSchema.deleteOne({
             guildId: guildId,
             allianceId: allianceId,
-        }, function (err, d) {
-            if (err) return res.status(400)
-            if (d.acknowledged && d.deletedCount == 1) {
-                message.reply(`${allianceId} has been removed from the database!`);
-            } else {
-                message.channel.send(`There was an error trying to delete ${allianceId} from the database. This may be because ${allianceId} is not in the database. If you are unsure please use the \`\`!alliancelist\`\` command!`);
-            }
         })
+
+        if (results.n === 0) {
+            message.channel.send(`There was an error trying to delete ${allianceId} from the database. This may be because ${allianceId} is not in the database. If you are unsure please use the \`\`!alliancelist\`\` command!`);
+        } else {
+            message.reply(`${allianceId} has been removed from the database!`);
+        }
 
 
     }
