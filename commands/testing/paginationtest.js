82643d6ca1aae00ea60221bb91c31a5787a7348f
Boomer5342 <uncertaindestiny@gmail.com>
2020-11-24 16:57:11 -0600

    Update paginationtest.js

--- commands/testing/paginationtest.js
+++ commands/testing/paginationtest.js
@@ -23,7 +23,7 @@ module.exports = {
          * @param {number} start The index to start from.
          */
         const generateEmbed = start => {
-        const current = allianceList.slice(start, start + 10)
+        const current = allianceList.slice(start, start + 5)
 
         // you can of course customise this embed however you want
         const embed = new Discord.MessageEmbed()
@@ -42,7 +42,7 @@ module.exports = {
         // send the embed with the first 10 guilds
         message.channel.send(generateEmbed(0)).then(message => {
         // exit if there is only one page of guilds (no need for all of this)
-        if (allianceList.length <= 10) return
+        if (allianceList.length <= 5) return
         // react with the right arrow (so that the user can click it) (left arrow isn't needed because it is the start)
         message.react('➡️')
         const collector = message.createReactionCollector(
@@ -57,13 +57,13 @@ module.exports = {
             // remove the existing reactions
             message.reactions.removeAll().then(async () => {
             // increase/decrease index
-            reaction.emoji.name === '⬅️' ? currentIndex -= 10 : currentIndex += 10
+            reaction.emoji.name === '⬅️' ? currentIndex -= 5 : currentIndex += 5
             // edit message with new embed
             message.edit(generateEmbed(currentIndex))
             // react with left arrow if it isn't the start (await is used so that the right arrow always goes after the left)
             if (currentIndex !== 0) await message.react('⬅️')
             // react with right arrow if it isn't the end
-            if (currentIndex + 10 < allianceList.length) message.react('➡️')
+            if (currentIndex + 5 < allianceList.length) message.react('➡️')
             })
         })
         })
