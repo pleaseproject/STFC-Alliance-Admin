0d2c876de921a6b6521692fafb758638bfb85879
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2020-11-29 12:28:18 -0600

    Update msg-logger.js

--- features/msg-logger.js
+++ features/msg-logger.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
 
     client.on('message', (message) => {
 
-        console.log(`${message.guild.name} => ${message.member.username}: ${message.content}`);
+        console.log(`${message.guild.name} => ${message.channel.name} => ${message.author.username}: ${message.content}`);
 
     });
 
