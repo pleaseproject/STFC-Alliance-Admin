43fa9f976aeb6955d3908576d45a82077551579f
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2020-11-29 12:59:36 -0600

    Update xpunignore.js

--- commands/misc/xpunignore.js
+++ commands/misc/xpunignore.js
@@ -21,11 +21,10 @@ module.exports = {
                 channelId: channelId,
             }
         }, function(err, doc) {
-            console.log(doc);
-            if (doc === null) {
-                message.reply(`Channel: ${channelId.name} is not an XP ignored channel!`);
-            } else {
+            if (doc.n > 0) {
                 message.reply(`Channel: ${channelId.name} has been removed from the XP ignore list!`);
+            } else {
+                message.reply(`Channel: ${channelId.name} is not an XP ignored channel!`);
             }
         });
 
