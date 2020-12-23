ab501ca1755c8d1beee81fa4986432dd0916c683
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2020-12-23 10:49:32 -0600

    Update slap.js

--- commands/misc/slap.js
+++ commands/misc/slap.js
@@ -1,19 +1,17 @@
 module.exports = {
     name: 'slap',
     description: 'Slaps mentioned user to get their attention',
+    alias: ['ninjakick'],
     category: 'Fun',
     cooldown: '5s',
     minArgs: 1,
-    maxArgs: -1, // -1 Means no limit
+    maxArgs: -1,
     expectedArgs: "<Target user's @>",
 
     run: (message, args) => {
 
         const sender = message.author.username;
         const target = message.mentions.users.first();
-        console.log(sender);
-        console.log(target);
-        console.log(args[0]);
 
         if (message.mentions.users.first() == null) 
         {
@@ -21,6 +19,10 @@ module.exports = {
         }
         else 
         {
+            if (message.Contains('ninjakick'))
+            {
+                console.log('ninjakick successful');
+            }
             message.channel.send(`${target}, you have been slapped by ${sender}.`)
         }
 
