470bd08f6c6f789b6f16fd9c8d6d0bce9d07b51c
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-01-05 22:21:45 -0600

    Update custom-command-reader.js

--- features/custom-command-reader.js
+++ features/custom-command-reader.js
@@ -11,7 +11,7 @@ module.exports = async (client) => {
             
             const results = await customCommandSchema.findOne({
                 guildId,
-                commandName
+                commandName: commandName
             })
 
             if (!results) {
