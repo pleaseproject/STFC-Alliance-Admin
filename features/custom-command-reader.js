6f8e76286c0ad009fbcddebdc5dc67d8137793ef
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-01-05 21:01:53 -0600

    Update custom-command-reader.js

--- features/custom-command-reader.js
+++ features/custom-command-reader.js
@@ -4,8 +4,8 @@ module.exports = (client) => {
 
     client.on('message', (message) => {
         //console.log(`${message.guild.name} => ${message.channel.name} => ${message.author.username}: ${message.content}`);
-        if (message.content.startsWith(`${prefix}test`)) {
-            console.log('TESTING COMMENCE.');
+        if (message.content.startsWith(prefix)) {
+            console.log(message.content.substring(1, message.content.indexOf(' ')));
         }
     })
 }
