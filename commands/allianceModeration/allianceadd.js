1740eab89f2e05b31eb51487ecb8df74b20bb040
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-01-05 19:46:31 -0600

    Transitioned code form Testaa to allianceadd

--- commands/allianceModeration/allianceadd.js
+++ commands/allianceModeration/allianceadd.js
@@ -6,39 +6,73 @@ module.exports = {
     name: 'allianceadd',
     aliases: ['alliancea', 'sadd', 'adds', 'allianceupdate', 'supdate', 'updates'],
     description: 'Adds new alliance to the database',
-    minArgs: 3,
-    maxArgs: -1,
+    expectedArgs: "<Alliance Tag>",
+    minArgs: 1,
+    maxArgs: 1,
 
     run: async ({ message, args }) => {
-
         const guildId = message.guild.id;
         allianceId = args[0].toLowerCase();
+        let arr = [];
+        const questions = [
+            `What is the status for \`\`${allianceId}\`\`?`,
+            `What is the reason for this status?`
+        ]
+        let counter = 0;
+        let filter = m => m.author.id === message.author.id;
 
-        const allianceStatus = {
-            allianceTag: args[0],
-            allianceStatus: args[1],
-            reason: args.slice(2).join(' '),
-            lastUpdated: new Date().getTime(),
-        }
-
-        await allianceSchema.findOneAndUpdate({
-            guildId: guildId,
-            allianceId: allianceId,
-        }, {
-
-            guildId: guildId,
-            allianceId: allianceId,
-            $push: {
-
-                allianceStatus: allianceStatus
-
-            }
-        }, {
-            upsert: true,
+        const collector = new discord.MessageCollector(message.channel, filter, {
+            max: questions.length,
+            time: 1000 * 45 // 45 seconds per answer?
         })
 
-        message.reply(`${allianceId} has been added/updated in the database!`);
+        message.channel.send(questions[counter++])
+        collector.on('collect', m => {
+            if (counter < questions.length) {
+                m.channel.send(questions[counter++])
+            }
+        })
 
+        collector.on('end', async collected => {
+            console.log(`Collected ${collected.size} messages`)
+
+            let counter = 0
+            let flag = true;
+            collected.forEach((value) => {
+                console.log(questions[counter++], value.content)
+                arr.push(value.content);
+            })
+
+            if (arr[0] != null && arr[1] != null) {
+                const allianceStatus = {
+                    allianceTag: allianceId,
+                    allianceStatus: arr[0],
+                    reason: arr[1],
+                    lastUpdated: new Date().getTime(),
+                }
+        
+                await drinkSchema.findOneAndUpdate({
+                    guildId: guildId,
+                    allianceId: allianceId,
+                }, {
+        
+                    guildId: guildId,
+                    allianceId: allianceId,
+                    $push: {
+        
+                        allianceStatus: allianceStatus
+        
+                    }
+                }, {
+                    upsert: true,
+                })
+        
+                message.reply(`${allianceId} has been added/updated in the database!`);  
+            } else {
+                message.reply(`There wasn an error during the adding of \`\`${allianceId}. This may be due to a time out! Please try your request again.`);
+            } 
+
+        })
     }
 
 }
