7c6c4266506a62c64a779034a69e3dfa19d98937
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-01-05 22:15:16 -0600

    Update custom-command-reader.js

--- features/custom-command-reader.js
+++ features/custom-command-reader.js
@@ -3,7 +3,7 @@ const customCommandSchema = require('../../schemas/customCommandSchema.js');
 
 module.exports = async (client) => {
 
-    client.on('message', (message) => {
+    client.on('message', async (message) => {
         //console.log(`${message.guild.name} => ${message.channel.name} => ${message.author.username}: ${message.content}`);
         if (message.content.startsWith(prefix)) {
             const guildId = message.guild.id;
