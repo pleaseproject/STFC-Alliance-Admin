78b1f9a908e353d1c6bbf9ca707cfc5a47546283
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-03-16 19:15:03 -0500

    Verification channel

---
+++ commands/misc/setverification.js
@@ -0,0 +1,90 @@
+
+const verificationSchema = require('../../schemas/verificationSchema')
+
+const cache = new Map()
+
+const loadData = async () => {
+  const results = await verificationSchema.find()
+
+  for (const result of results) {
+    cache.set(result._id, result.channelId)
+  }
+}
+loadData()
+
+module.exports = {
+    requiredPermissions: ['ADMINISTRATOR'],
+    category: 'Util',
+    run: async ({ message, args }) => {
+
+        if (args.length !== 2) {
+            message
+                .reply('You must provide an emoji to react with and a role ID')
+                .then((message) => {
+                    message.delete({
+                        timeout: 1000 * seconds,
+                    })
+                })
+
+            message.delete()
+            return
+        }
+
+        const { guild, channel } = message
+
+        let emoji = args[0]
+        if (emoji.includes(':')) {
+            const split = emoji.split(':')
+            const emojiName = split[1]
+
+            emoji = guild.emojis.cache.find((emoji) => {
+                return emoji.name === emojiName
+            })
+        }
+        const roleId = args[1]
+        const role = guild.roles.cache.get(roleId)
+
+        if (!role) {
+            message
+                .reply('That role does not exist')
+                .then(message => {
+                    message.delete({
+                        timeout: 1000 * seconds,
+                    })
+                })
+
+            message.delete()
+            return
+        }
+
+        message.delete().then(() => {
+            channel.message.fetch({ limit: 1}).then(async results => {
+                const firstMessage = results.first()
+
+                if (!firstMessage) {
+                    channel.send('There is no message to react to').then(message => {
+                        message.delete({
+                            timeout: 1000 * seconds
+                        })
+                    })
+                    return
+                }
+                firstMessage.react(emoji)
+                
+                await verificationSchema.findOneAndUpdate({
+                    _id: guild.id
+                }, {
+                   _id: guild.id,
+                   channelId: channel.id,
+                   roleId: roleId 
+                }, {}, {})
+            })
+        })
+
+    message.reply('Rule Verification Channel Set!')
+  }
+}
+
+module.exports.getChannelId = (guildId) => {
+  return cache.get(guildId)
+}
---
+++ schemas/verificationSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const reqString = {
+  type: String,
+  required: true,
+}
+
+const verificationSchema = mongoose.Schema({
+  // Guild ID
+  _id: reqString,
+  channelId: reqString,
+  roleId: reqString,
+})
+
+module.exports = mongoose.model('verification-channels',verificationSchema)
