0a712a0df21209b003031e947ae6a68aa6a16015
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-03-16 19:57:03 -0500

    Update setverification.js

--- commands/misc/setverification.js
+++ commands/misc/setverification.js
@@ -59,31 +59,35 @@ module.exports = {
         }
 
         message.delete().then(() => {
-            channel.message.fetch({ limit: 1}).then(async results => {
-                const firstMessage = results.first()
-
-                if (!firstMessage) {
-                    channel.send('There is no message to react to').then(message => {
-                        message.delete({
-                            timeout: 1000 * seconds
-                        })
-                    })
-                    return
-                }
-                firstMessage.react(emoji)
-                
-                await verificationSchema.findOneAndUpdate({
-                    _id: guild.id
-                }, {
-                    _id: guild.id,
-                    channelId: channel.id,
-                    roleId: roleId 
-                }, {
-                    upsert: true,
+            channel.messages.fetch({ limit: 1 }).then(async (results) => {
+              const firstMessage = results.first()
+      
+              if (!firstMessage) {
+                channel.send('There is no message to react to').then((message) => {
+                  message.delete({
+                    timeout: 1000 * seconds,
+                  })
                 })
+      
+                return
+              }
+      
+            firstMessage.react(emoji)
+            await verificationSchema.findOneAndUpdate(
+            {
+                _id: guild.id,
+            },
+            {
+                _id: guild.id,
+                channelId: channel.id,
+                roleId,
+            },
+            {
+                upsert: true,
             })
         })
 
     message.reply('Rule Verification Channel Set!')
+    })
   }
 }
