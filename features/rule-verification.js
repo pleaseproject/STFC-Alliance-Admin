e9e58e14c7d88d06d798596c7312bdf17944c2df
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-03-28 15:59:15 -0500

    Update rule-verification.js

--- features/rule-verification.js
+++ features/rule-verification.js
@@ -37,7 +37,7 @@ module.exports = (client) => {
     if (roleId) {
       const { guild } = reaction.message
       const member = guild.members.cache.get(user.id)
-      member.roles.add(roleId)
+      member.roles.add(776639146403299351)
       console.log(`Assigning Role { ${roleId} } to { ${member} }`)
     }
   })
