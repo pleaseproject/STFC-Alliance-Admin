3d30169d1db8d6ec01459c27286a2e04a1a8ca46
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-03-28 19:31:11 -0500

    Update roll.js

--- commands/misc/roll.js
+++ commands/misc/roll.js
@@ -8,13 +8,13 @@ module.exports = {
     run: ({ message, args }) => {
 
         if (args.length < 1) {
-            let tempNum = Math.floor(Math.random() * (100 - 1) + 1);
-            message.reply(`Rolling a random number between 1-100! \n You have rolled ${tempNum}!`);
+            let tempNum = Math.floor(Math.random() * 100 + 1);
+            message.reply(`Rolling a random number between 1-100! \n You have rolled a: \`\`${tempNum}!\`\``);
         } else {
             if(!isNaN(args[0])) {
                 let x = args[0];
                 let tempNum = Math.floor(Math.random() * x + 1);
-                message.reply(`Rolling a random number between 1-${x}! \n You have rolled ${tempNum}!`);
+                message.reply(`Rolling a random number between 1-${x}! \n You have rolled a: \`\`${tempNum}!\`\``);
             } else {
                 message.reply(`The arguement \`\`${args[0]}\`\` is not a number. Please try the command again with a number!`);
             }
