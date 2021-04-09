fd478841267da041c22345bb33c24a8788b729ba
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-04-08 21:16:49 -0500

    Update setScheduledMessageChannel.js

--- commands/misc/setScheduledMessageChannel.js
+++ commands/misc/setScheduledMessageChannel.js
@@ -1,30 +1,41 @@
 const scheduledMessageChannelSchema = require('../../schemas/scheduledMessageChannelSchema');
 
+const cache = new Map();
+
+const loadData = async () => {
+    const results = await scheduledMessageChannelSchema.find();
+
+    for (const result of results) {
+        cache.set(result._id, result.channelId);
+    }
+}
+loadData();
+
 module.exports = {
     requiredPermissions: ['ADMINISTRATOR'],
     category: 'Util',
     name: 'setScheduledMessageChannel',
     aliases: ['setsch', ],
     run: async (message) => {
-    const { guild, channel } = message
-    console.log(`HERE IS THE CHANNEL ID: ${channel.id}`)
-    await scheduledMessageChannelSchema.findOneAndUpdate(
-      {
-        _id: guild.id,
-      },
-      {
-        _id: guild.id,
-        channelId: channel.id,
-      },
-      {
-        upsert: true,
-      }
-    );
+        const guildId = message.guild.id;
+        const channelId = message.channel.id;
+        console.log(`HERE IS THE CHANNEL ID: ${channelId}`)
+        await scheduledMessageChannelSchema.findOneAndUpdate(
+            {
+                _id: guildId,
+            },
+            {
+                _id: guildId,
+                channelId: channelId,
+            },
+            {
+                upsert: true,
+            });
 
-    cache.set(guild.id, channel.id);
+        cache.set(guildId, channelId);
 
-    message.reply('Scheduled Message Channel Set');
-  },
+        message.reply('Scheduled Message Channel Set');
+    },
 }
 
 module.exports.getChannelId = (guildId) => {
