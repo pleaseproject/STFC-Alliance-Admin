f5e6577727cdfc59017cb73288c87a1e210a986a
Boomer5342 <61943342+Boomer5342@users.noreply.github.com>
2021-04-11 18:53:11 -0500

    Update welcome.js

--- features/welcome.js
+++ features/welcome.js
@@ -1,7 +1,7 @@
 const Canvas = require("canvas");
 const { MessageAttachment } = require("discord.js");
 const path = require("path");
-const { getChannelId } = require("../commands/misc/setwelcome");
+const { getChannelId } = require("../commands/administrative/setwelcome");
 
 module.exports = (client) => {
   client.on("guildMemberAdd", async (member) => {
